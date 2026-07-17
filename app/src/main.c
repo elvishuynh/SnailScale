e7156f1c00797c6c202d81cc5e6e5c9db751d90e
elvishuynh <hello@elvishuynh.com>
2026-07-17 03:24:42 -0700

    - Update CMakeLists.txt to generate FLPR firmware header and adjust include directories. - Modify prj.conf to enable Bluetooth and MCUmgr configurations for OTA DFU. - Refactor bin_to_header.py to use argparse - remove unused MCUboot configuration files.

--- app/src/main.c
+++ app/src/main.c
@@ -18,17 +18,17 @@ LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 #define FLPR_SRAM_GLOBAL_ADDR (DT_REG_ADDR(DT_NODELABEL(cpuflpr_sram_code_data)))
 
 static void flpr_start(void) {
-	// power up ram
-	power_up_ram(FLPR_SRAM_GLOBAL_ADDR, FLPR_SRAM_GLOBAL_ADDR + FLPR_FIRMWARE_SIZE);
-	
-	// copy firmware
-	memcpy((void *)FLPR_SRAM_GLOBAL_ADDR, flpr_firmware, FLPR_FIRMWARE_SIZE);
-	
-	// set pc
-	nrf_vpr_initpc_set(NRF_VPR00, FLPR_SRAM_GLOBAL_ADDR);
-	
-	// start core
-	nrf_vpr_cpurun_set(NRF_VPR00, true);
+    // Power up RAM
+    power_up_ram(FLPR_SRAM_GLOBAL_ADDR, FLPR_SRAM_GLOBAL_ADDR + FLPR_FIRMWARE_SIZE);
+    
+    // Copy the embedded FLPR firmware array into the allocated SRAM space
+    memcpy((void *)FLPR_SRAM_GLOBAL_ADDR, flpr_firmware, FLPR_FIRMWARE_SIZE);
+    
+    // Set Program Counter for the VPR core to the SRAM address
+    nrf_vpr_initpc_set(NRF_VPR00, FLPR_SRAM_GLOBAL_ADDR);
+    
+    // Start the VPR core
+    nrf_vpr_cpurun_set(NRF_VPR00, true);
 }
 
 int main(void)
